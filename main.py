d9f44b11038943149e0c1923dd8d35bfcea2a5bf
mikumifa <1055069518@qq.com>
2023-11-07 00:55:41 +0800

    inti

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from loginIn import login_request
 from punchIn import punch_in
 
 if __name__ == "__main__":
-    response = punch_in(session=login_request("mikumifa", "ldxy041015"))
+    response = punch_in(session=login_request("xxx", "xxx"))
     print(response.text)
