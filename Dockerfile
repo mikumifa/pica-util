605fac3ee3a866d54c30411b91be47859c7b4d7e
mikumifa <1055069518@qq.com>
2023-11-07 14:30:42 +0800

    添加dockerfile crontab定死事务

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# 使用官方的 Python 镜像作为基础镜像
+FROM python:3.11
+
+# 设置工作目录
+WORKDIR /app
+
+# 复制你的 Python 文件到镜像中
+COPY . /app/
+
+
+RUN pip install -r requirements.txt
+
+# 安装 cron
+RUN apt-get update && apt-get install -y cron
+
+# 复制你的 crontab 文件到容器中
+COPY crontab /etc/cron.d/my-cron
+
+
+# 添加执行权限
+RUN chmod 0644 /etc/cron.d/my-cron
+
+# 启动 cron 服务
+CMD ["cron", "-f"]
--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+import json
+
 from loginIn import login_request
 from punchIn import punch_in
 
+
+def read_config(filename):
+    try:
+        with open(filename, "r") as file:
+            config_data = json.load(file)
+            return config_data
+    except FileNotFoundError:
+        print(f"Config file '{filename}' not found.")
+        return None
+
+
 if __name__ == "__main__":
-    response = punch_in(session=login_request("xxx", "xxx"))
-    print(response.text)
+    config = read_config("config.json")
+    if config:
+        for user_config in config:
+            username = user_config.get("username")
+            password = user_config.get("password")
+
+            if username and password:
+                response = punch_in(session=login_request(username, password))
+                print(f"Username: {username}, Response: {response.text}")
+            else:
+                print("Username or password missing in config.")
+    else:
+        print("No user configurations found in config file.")
